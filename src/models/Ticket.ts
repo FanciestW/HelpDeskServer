80f5e7568610208b5d904b7d1ec5508a44c5dfc5
William Lin <wlin26@yahoo.com>
2020-02-17 11:28:05 -0500

    Updated ticket model to validate no empty string for title

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 import ITicket from '../interfaces/Ticket';
 import User from './User';
 
-const validateUserExists = async (uid) => {
+const validateUserExists = async (uid: string) => {
   if (uid === '') {
     return true;
   } else if (await User.countDocuments({ uid, }) > 0) {
@@ -14,7 +14,16 @@ const validateUserExists = async (uid) => {
 
 export const TicketSchema: Schema = new Schema({
   ticketId: { type: String, required: true, unique: true },
-  title: { type: String, required: true, },
+  title: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (titleStr: string) => {
+        return titleStr.length > 0;
+      },
+      message: 'A blank title is not allowed',
+    }
+  },
   description: { type: String, required: false, default: '' },
   createdBy: { 
     type: String,
