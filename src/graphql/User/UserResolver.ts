377d01c908b2d07a9bc4573b1dbc05bb067bba58
William Lin <wlin26@yahoo.com>
2020-02-25 23:56:20 -0500

    Added new user and update user mutations

--- src/graphql/User/UserResolver.ts
+++ src/graphql/User/UserResolver.ts
@@ -9,6 +9,14 @@ export const UserResolver = {
       return await User.findOne({ uid: args.uid });
     }
   },
+  Mutation: {
+    newUser: async (_, args) => {
+      return await User.create(args);
+    },
+    updateUser: async (_, args) => {
+      return User.findOneAndUpdate({ uid: args.uid }, args, { new: true, upsert: true, });
+    }
+  },
   User: {
     createdAt: (obj) => {
       const date: Date = obj.createdAt;
