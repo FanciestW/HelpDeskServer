b533972a978db8a0443cc30dd81f1b59da5ffd7e
William Lin <wlin26@yahoo.com>
2020-03-02 11:33:32 -0500

    Updated strong parameters middleware
    Needs testing

--- src/middleware/StrongParam.ts
+++ src/middleware/StrongParam.ts
@@ -1,1 +1,15 @@
-import express from 'express';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+
+export default (types: Object): RequestHandler => {
+  return function(req: Request, res: Response, next: NextFunction) {
+    res.locals.body = {};
+    Object.keys(types).forEach((key) => {
+      const reqObj = req.body[key];
+      if (typeof reqObj === types[key]) { 
+        res.locals.body[key] = reqObj;
+      }
+    });
+    delete req.body;
+    return next();
+  };
+};
