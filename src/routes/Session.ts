e49fe9cbb8205ea7051d8d77f77489d4476628d4
William Lin <wlin26@yahoo.com>
2020-03-04 15:42:20 -0500

    Added bcrypt and finished session routes

--- src/routes/Session.ts
+++ src/routes/Session.ts
@@ -1,18 +1,83 @@
 // eslint-disable-next-line no-unused-vars
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
+import bcrypt from 'bcrypt';
+import nanoid from 'nanoid';
+import StrongParams from '../middleware/StrongParam';
 import SessionMiddleware from '../middleware/AuthSession';
+import StrongParam from '../middleware/StrongParam';
+import User from '../models/User';
+import Session from '../models/Session';
 const router = express.Router();
 
-router.post('/logout', SessionMiddleware, (req: Request, res: Response) => {
+router.post('/logout', SessionMiddleware, async (_, res: Response) => {
+  const sid = res.locals.sessionCookie.sid;
+  await Session.findOneAndDelete({ sid, });
   return res.status(200).send('Session Logout');
 });
 
-router.post('/new', (req: Request, res: Response) => {
-  return res.status(200).send('New Session Created');
+const loginStrongParams = {
+  email: 'string',
+  password: 'string'
+};
+router.post('/login', StrongParams(loginStrongParams), async (_, res: Response) => {
+  try {
+    const { email, password } = res.locals.body;
+    const user = await User.findOne({ email });
+    if (await bcrypt.compare(password, user.passwordDigest)) {
+      return res.sendStatus(200);
+    } else {
+      return res.sendStatus(401);
+    }
+  } catch (err) {
+    return res.sendStatus(500);
+  }
 });
 
-router.post('/check', SessionMiddleware, (req: Request, res: Response) => {
-  return res.status(200).send('Session Check');
+const signUpStrongParams = {
+  firstName: 'string',
+  lastName: 'string',
+  middleName: 'string',
+  password: 'string',
+  email: 'string',
+  phone: 'string',
+  company: 'string',
+  isTechnician: 'boolean'
+};
+router.post('/signup', StrongParam(signUpStrongParams), async (_, res: Response) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      middleName,
+      password,
+      email,
+      phone,
+      company,
+      isTechnician
+    } = res.locals.body;
+    const newUser = await new User({
+      firstName,
+      lastName,
+      middleName,
+      password,
+      email,
+      phone,
+      company,
+      isTechnician
+    }).save();
+    if (newUser) {
+      let sid = '';
+      do {
+        sid = nanoid();
+      } while (!(await Session.findOne({ sid, })));
+      await new Session({ uid: newUser.uid, sid }).save();
+      return res.status(200).cookie('session', sid, { maxAge: 86400000 }).send(newUser);
+    } else {
+      throw new Error('Unable to create new User');
+    }
+  } catch (err) {
+    return res.sendStatus(500);
+  }
 });
 
 export default router;
